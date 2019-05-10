6477b0295d0ec92b503243085a3576f54b78acd7
zuomoumou666 <zuomoumou666@gmail.com>
2019-05-11 00:25:10 +0800

    7. Reverse Integer: don't use arr.reverse()

--- leetcode/7. Reverse Integer.js
+++ leetcode/7. Reverse Integer.js
@@ -2,14 +2,35 @@
  * @param {number} x
  * @return {number}
  */
+// var reverse = function (x) {
+//   const max = Math.pow(2, 31) - 1;
+//   const min = - Math.pow(2, 31);
+//   const str = Math.abs(x) + '';
+//   let result = str.split('').reverse().join('');
+//   result = x < 0 ? 0 - result : result - 0;
+//   if (result < min || result > max) return 0;
+//   return result;
+// };
+
+// var reverse = function (x) {
+//   const max = Math.pow(2, 31) - 1;
+//   const min = - Math.pow(2, 31);
+//   let result = parseInt((Math.abs(x) + "").split('').reverse().join(''), 10) * Math.sign(x);
+//   if (result < min || result > max) return 0;
+//   return result;
+// };
+
 var reverse = function (x) {
   const max = Math.pow(2, 31) - 1;
   const min = - Math.pow(2, 31);
   const str = Math.abs(x) + '';
-  let result = str.split('').reverse().join('');
-  result = x < 0 ? 0 - result : result - 0;
+  const len = str.length;
+  let newStr = '';
+  for (let i = len - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+  let result = parseInt(newStr, 10) * Math.sign(x);
   if (result < min || result > max) return 0;
   return result;
 };
-
 console.log(reverse(-1563847412));
