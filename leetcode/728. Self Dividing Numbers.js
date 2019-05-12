b98beeb105073c437eb93e0761fd03c5ed1851f6
zuomoumou666 <zuomoumou666@gmail.com>
2019-05-12 23:49:17 +0800

    update

--- leetcode/728. Self Dividing Numbers.js
+++ leetcode/728. Self Dividing Numbers.js
@@ -21,27 +21,43 @@
  * @param {number} right
  * @return {number[]}
  */
-var selfDividingNumbers = function (left, right) {
-  if (left < 1 || right > 10000) return null;
-  const result = [];
-  for (let i = left; i <= right; i++) {
-    if (isSelfDividing(i)) {
-      result.push(i);
+// var selfDividingNumbers = function (left, right) {
+//   if (left < 1 || right > 10000) return null;
+//   const result = [];
+//   for (let i = left; i <= right; i++) {
+//     if (isSelfDividing(i)) {
+//       result.push(i);
+//     }
+//   }
+//   return result;
+// };
+
+// function isSelfDividing(num) {
+//   let arr = (num + '').split('');
+//   const len = arr.length;
+//   for (let i = 0; i < len; i++) {
+//     if (arr[i] === '0') return false;
+//     if (num % (arr[i] - 0) !== 0) return false;
+//   }
+//   return true;
+// }
+
+
+var selfDividingNumbers = function(left, right) {
+  let r,d;
+  const nums = [];
+
+  for (let num=left; num<=right; num+=1) {
+    d=num;
+    r=num%10;
+    while (r && !(num%r)) {
+      d=Math.floor(d/10);
+      r=d%10;
     }
+    if (d===0) nums.push(num);
   }
-  return result;
+  return nums;
 };
-
-function isSelfDividing(num) {
-  let arr = (num + '').split('');
-  const len = arr.length;
-  for (let i = 0; i < len; i++) {
-    if (arr[i] === '0') return false;
-    if (num % (arr[i] - 0) !== 0) return false;
-  }
-  return true;
-}
-
 
 const left = 1, right = 22;
 
