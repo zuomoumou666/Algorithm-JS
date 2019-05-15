2c7e2ca4d78e67f4f45e54e9363b1135bb192e99
zuomoumou666 <zuomoumou666@gmail.com>
2019-05-15 22:41:48 +0800

    451. Sort Characters By Frequency

---
+++ leetcode/451. Sort Characters By Frequency.js
@@ -0,0 +1,71 @@
+// 451. Sort Characters By Frequency
+
+// Given a string, sort it in decreasing order based on the frequency of characters.
+
+// Example 1:
+
+// Input:
+// "tree"
+
+// Output:
+// "eert"
+
+// Explanation:
+// 'e' appears twice while 'r' and 't' both appear once.
+// So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+// Example 2:
+
+// Input:
+// "cccaaa"
+
+// Output:
+// "cccaaa"
+
+// Explanation:
+// Both 'c' and 'a' appear three times, so "aaaccc" is also a valid answer.
+// Note that "cacaca" is incorrect, as the same characters must be together.
+// Example 3:
+
+// Input:
+// "Aabb"
+
+// Output:
+// "bbAa"
+
+// Explanation:
+// "bbaA" is also a valid answer, but "Aabb" is incorrect.
+// Note that 'A' and 'a' are treated as two different characters.
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var frequencySort = function (s) {
+  if (!s) return s;
+  const memory = {};
+  const arr = [];
+  let string = "";
+  for (let i = 0; i < s.length; i++) {
+    const curS = s[i]
+    memory[curS] = memory[curS] ? memory[curS] + 1 : 1;
+  }
+  for (let k in memory) {
+    let str = "";
+    let num = memory[k];
+    while (num) {
+      str += k;
+      num--
+    }
+    arr[memory[k]] = (arr[memory[k]] || '') + str;
+  }
+  for (let j = arr.length - 1; j > 0; j--) {
+    if (arr[j]) {
+      string = string + arr[j];
+    }
+  }
+  return string;
+};
+
+// const input = 'tree';
+// const input = 'cccaaa';
+const input = 'Aabb';
+console.log(frequencySort(input));
