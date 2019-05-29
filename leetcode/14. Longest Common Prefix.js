4726a7772567a855d29470c0a164eabdf80f77a3
zuomoumou666 <zuomoumou666@gmail.com>
2019-05-30 00:09:53 +0800

    use reduce

--- leetcode/14. Longest Common Prefix.js
+++ leetcode/14. Longest Common Prefix.js
@@ -20,23 +20,33 @@
  * @param {string[]} strs
  * @return {string}
  */
+// var longestCommonPrefix = function (strs) {
+//   if (!strs.length) return '';
+//   let arr = strs[0].split("");
+//   for (let i = 1, len = strs.length; i < len; i++) {
+//     const str = strs[i];
+//     if (arr.length) {
+//       for (let j = 0, jLen = arr.length; j < jLen; j++) {
+//         if (str[j] !== arr[j]) {
+//           arr = arr.slice(0, j);
+//           break;
+//         }
+//       }
+//     } else {
+//       break;
+//     }
+//   }
+//   return arr.join("");
+// };
 var longestCommonPrefix = function (strs) {
-  if (!strs.length) return '';
-  let arr = strs[0].split("");
-  for (let i = 1, len = strs.length; i < len; i++) {
-    const str = strs[i];
-    if (arr.length) {
-      for (let j = 0, jLen = arr.length; j < jLen; j++) {
-        if (str[j] !== arr[j]) {
-          arr = arr.slice(0, j);
-          break;
-        }
-      }
-    } else {
-      break;
+  if (strs === undefined || strs.length === 0) { return ''; }
+  return strs.reduce((pre, cur) => {
+    let i = 0;
+    while (pre && cur && i < pre.length && pre[i] === cur[i]) {
+      i++;
     }
-  }
-  return arr.join("");
+    return pre.slice(0, i)
+  })
 };
 
 const ipt = ["flower", "flow", "flight"];
