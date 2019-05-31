017e89d67396a945318a12a5040c480b8af1d443
zuomoumou666 <zuomoumou666@gmail.com>
2019-05-31 22:15:23 +0800

    13. Roman to Integer

--- leetcode/26. Remove Duplicates from Sorted Array.js
+++ leetcode/26. Remove Duplicates from Sorted Array.js
@@ -53,17 +53,27 @@
 //   }
 // };
 
+// var removeDuplicates = function (nums) {
+//   if (!nums.length) return false;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] === nums[i + 1]) {
+//       nums.splice(i, 1);
+//       i--;
+//     }
+//   }
+// };
+
 var removeDuplicates = function (nums) {
-  if (!nums.length) return false;
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === nums[i + 1]) {
-      nums.splice(i, 1);
-      i--;
+  let i = 0;
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] != nums[i]) {
+      nums[++i] = nums[j];
     }
   }
+  return ++i;
 };
 
 // const nums = [1, 1, 2];
 const nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
 removeDuplicates(nums)
-console.log(nums);
+console.log(JSON.stringify(nums));
