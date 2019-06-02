49d24790687c69622e00a604c16b707b7b6a62c8
zuomoumou666 <zuomoumou666@gmail.com>
2019-06-02 14:50:04 +0800

    20. Valid Parentheses

---
+++ leetcode/20. Valid Parentheses.js
@@ -0,0 +1,66 @@
+// 20. Valid Parentheses
+
+// Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Note that an empty string is also considered valid.
+
+// Example 1:
+
+// Input: "()"
+// Output: true
+// Example 2:
+
+// Input: "()[]{}"
+// Output: true
+// Example 3:
+
+// Input: "(]"
+// Output: false
+// Example 4:
+
+// Input: "([)]"
+// Output: false
+// Example 5:
+
+// Input: "{[]}"
+// Output: true
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  if (s.length % 2 !== 0) return false;
+  const arr = [];
+  const hash = {
+    '(': ')',
+    '{': '}',
+    '[': ']',
+  };
+  for (let i = 0, len = s.length; i < len; i++) {
+    const cur = s[i];
+    if (hash[cur]) {
+      arr.push(hash[cur]);
+    } else {
+      const last = arr.pop();
+      if (!last || last !== cur) {
+        return false;
+      }
+    }
+  }
+  return arr.length === 0;
+};
+
+// const ipt = '()';
+// const ipt = '()[]{}';
+// const ipt = "(]";
+// const ipt = "([)]";
+// const ipt = "{[]}";
+// const ipt = "";
+// const ipt = "((";
+const ipt = "))";
+console.log(isValid(ipt));
