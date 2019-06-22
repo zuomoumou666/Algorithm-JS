e3520f098cb17f385997171db7a52b0f805d4489
zuomoumou666 <zuomoumou666@gmail.com>
2019-06-22 21:46:31 +0800

    other solutions

--- leetcode/125. Valid Palindrome.js
+++ leetcode/125. Valid Palindrome.js
@@ -18,42 +18,81 @@
  * @param {string} s
  * @return {boolean}
  */
-const add = (n) => () => {
-  n++;
-  return n;
-};
-const dec = (n) => () => {
-  n--;
-  return n;
-};
+// const add = (n) => () => {
+//   n++;
+//   return n;
+// };
+// const dec = (n) => () => {
+//   n--;
+//   return n;
+// };
+// var isPalindrome = function (s) {
+//   let start = 0;
+//   let end = s.length - 1;
+//   while (start <= end) {
+//     let sS, sE;
+//     [sS, start] = getStr(s, add(start - 1));
+//     [sE, end] = getStr(s, dec(end + 1));
+//     if (sS !== sE) return false;
+//     start++;
+//     end--;
+//   }
+//   return true;
+// };
+
+
+// function getStr(str, fn) {
+//   let index = fn();
+//   if (index >= str.length || index < 0) return ['', index];
+//   if (/\w/.test(str[index])) {
+//     return [str[index].toLowerCase(), index]
+//   }
+//   return getStr(str, fn);
+// }
+
+// var isPalindrome = function (s) {
+//   let n = 0;
+//   let len = s.length;
+//   let arr = [];
+//   while (n < len) {
+//     if (/\w/.test(s[n])) { arr.push(s[n].toLowerCase()) }
+//     n++;
+//   }
+//   let start = 0;
+//   let end = arr.length - 1;
+//   while (start <= end) {
+//     let sS = arr[start];
+//     let sE = arr[end];
+//     if (sS !== sE) return false;
+//     start++;
+//     end--;
+//   }
+//   return true;
+// };
+// var isPalindrome = function (s) {
+//   s = s.replace(/\W/g, "");
+//   let start = 0;
+//   let end = s.length - 1;
+//   while (start <= end) {
+//     let sS = s[start];
+//     let sE = s[end];
+//     if (sS.toLowerCase() !== sE.toLowerCase()) return false;
+//     start++;
+//     end--;
+//   }
+//   return true;
+// };
 var isPalindrome = function (s) {
-  let start = 0;
-  let end = s.length - 1;
-  while (start <= end) {
-    let sS, sE;
-    [sS, start] = getStr(s, add(start - 1));
-    [sE, end] = getStr(s, dec(end + 1));
-    if (sS !== sE) return false;
-    start++;
-    end--;
-  }
-  return true;
+  s = s.replace(/\W/g, "");
+  const r = s.split("").reverse().join("");
+  return r.toLowerCase() === s.toLowerCase();
 };
 
 
-function getStr(str, fn) {
-  let index = fn();
-  if (index >= str.length || index < 0) return ['', index];
-  if (/\w/.test(str[index])) {
-    return [str[index].toLowerCase(), index]
-  }
-  return getStr(str, fn);
-}
 
-
-// const Input = "A man, a plan, a canal: Panama";
+const Input = "A man, a plan, a canal: Panama";
 // const Input = "race a car";
-const Input = " ";
+// const Input = " ";
 console.log(isPalindrome(Input));
 
 // console.log(getStr(Input, add(-1)));
@@ -62,3 +101,4 @@ console.log(isPalindrome(Input));
 // let a = add(1);
 // console.log(a());
 // console.log(a());
+
