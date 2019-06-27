ec1cc8df11697166c664182e3ce00be33fa4f574
zuomoumou666 <zuomoumou666@gmail.com>
2019-06-28 00:45:49 +0800

    122. Best Time to Buy and Sell Stock II

--- leetcode/122. Best Time to Buy and Sell Stock II.js
+++ leetcode/122. Best Time to Buy and Sell Stock II.js
@@ -23,23 +23,77 @@
 // Input: [7,6,4,3,1]
 // Output: 0
 // Explanation: In this case, no transaction is done, i.e. max profit = 0.
-
+// dp = Math.max(dp[i+1]+dp[i],)
 
 /**
  * @param {number[]} prices
  * @return {number}
  */
+// var maxProfit = function (prices) {
+//   let profit = 0;
+//   let i = 0;
+//   let len = prices.length;
+//   while (i < len) {
+//     if (prices[i + 1] <= prices[i]) {
+//       i++;
+//       continue;
+//     } else {
+//       let j = i + 1;
+//       while (j < len && prices[j] > prices[j - 1]) {
+//         j++;
+//       }
+//       profit += prices[j - 1] - prices[i];
+//       i = j;
+//     }
+//   }
+//   return profit;
+// };
+// var maxProfit = function (prices) {
+//   let profit = 0;
+//   let i = 0;
+//   let len = prices.length;
+//   let valley = prices[0];
+//   let peak = prices[0];
+//   while (i < len - 1) {
+//     while (i < len - 1 && prices[i + 1] <= prices[i]) {
+//       i++;
+//     }
+//     valley = prices[i]
+//     while (i < len - 1 && prices[i + 1] >= prices[i]) {
+//       i++;
+//     }
+//     peak = prices[i];
+//     profit += peak - valley;
+//   }
+//   return profit;
+// };
 var maxProfit = function (prices) {
   let profit = 0;
-  for (let i = 0, len = prices.length; i < len; i++) {
-    for (let j = i + 1; j < len; j++) {
-      profit = Math.max(profit, prices[j] - prices[i]);
+  let i = 1;
+  let len = prices.length;
+  while (i < len) {
+    if (prices[i] >= prices[i - 1]) {
+      profit += prices[i] - prices[i - 1];
     }
+    i++;
   }
   return profit;
 };
-
+// [0, -6, -2, -4, -1, -3];
+// [0, 0, 4, 2, 5, 3]
+// [0, 0, 0, -2, 1, -1]
+// [0, 0, 0, 0, 3, 1]
+// [0, 0, 0, 0, 0, -2]
+// [0, 0, 0, 0, 0, 0]
+// -----------------------
+//   [0, 1, 2, 3, 4]
+//   [0, 0, 1, 2, 3]
+//   [0, 0, 0, 1, 2]
+//   [0, 0, 0, 0, 1]
 const Input = [7, 1, 5, 3, 6, 4];
 // const Input = [1, 2, 3, 4, 5];
+// const Input = [6, 1];
+
+[-6, 4, -2, 3, -2]
 
 console.log(maxProfit(Input));
