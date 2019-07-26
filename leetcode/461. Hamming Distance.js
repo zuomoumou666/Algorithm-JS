dab25d875b800e648fd3ec6fd416608bbf57ca8f
zuomoumou666 <zuomoumou666@gmail.com>
2019-07-27 00:07:19 +0800

    461. Hamming Distance

--- leetcode/461. Hamming Distance.js
+++ leetcode/461. Hamming Distance.js
@@ -3,7 +3,7 @@
 
 // The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
 
-// Given two integers x and y, calculate the Hamming distance.
+// Given two integers x and X, calculate the Hamming distance.
 
 // Note:
 // 0 ≤ x, y < 231.
@@ -30,11 +30,13 @@
 var hammingDistance = function (x, y) {
   let binaryX = x.toString(2).split('');
   let binaryY = y.toString(2).split('');
-  let lenX = binaryX.length;
-  let lenY = binaryY.length;
-  const getStr = (i, binary) => binary[i] === undefined ? '0' : binary[i];
-  for (let i = len - 1; i >= 0; i--) {
-    if (getStr(i, binaryX) !== getStr(i, binaryY)) {
+  let maxLen = Math.max(binaryX.length, binaryY.length);
+  let result = 0;
+  const prefixArr = (arr, len) => (new Array(len - arr.length).fill('0').concat(arr));
+  binaryX = prefixArr(binaryX, maxLen);
+  binaryY = prefixArr(binaryY, maxLen);
+  for (let i = 0; i < maxLen; i++) {
+    if (binaryX[i] !== binaryY[i]) {
       result++;
     }
   }
