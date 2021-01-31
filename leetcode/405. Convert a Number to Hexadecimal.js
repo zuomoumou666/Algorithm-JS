5aa3cd1c09742ee7c000e5f5dd02651ba32a21ef
zuomoumou666 <zuomoumou666@gmail.com>
2021-01-31 22:38:49 +0800

    405. Convert a Number to Hexadecimal

---
+++ leetcode/405. Convert a Number to Hexadecimal.js
@@ -0,0 +1,52 @@
+// 405. Convert a Number to Hexadecimal
+// Easy
+
+// Given an integer, write an algorithm to convert it to hexadecimal. For negative integer, two’s complement method is used.
+
+// Note:
+
+// All letters in hexadecimal (a-f) must be in lowercase.
+// The hexadecimal string must not contain extra leading 0s. If the number is zero, it is represented by a single zero character '0'; otherwise, the first character in the hexadecimal string will not be the zero character.
+// The given number is guaranteed to fit within the range of a 32-bit signed integer.
+// You must not use any method provided by the library which converts/formats the number to hex directly.
+// Example 1:
+
+// Input:
+// 26
+
+// Output:
+// "1a"
+// Example 2:
+
+// Input:
+// -1
+
+// Output:
+// "ffffffff"
+
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var toHex = function (num) {
+  if (num === 0) return '0';
+  let result = '';
+  if (num < 0) {
+    num += Math.pow(2, 32);
+  }
+  while (num >= 1) {
+    const cur = num % 16;
+    result = convert(cur) + result;
+    num = (num - cur) / 16;
+  }
+  return result;
+};
+
+function convert(n) {
+  if (n > 16) return null;
+  const hexMap = ['a', 'b', 'c', 'd', 'e', 'f'];
+  if (n < 10) return n + '';
+  return hexMap[n - 10];
+}
+
+console.log(toHex(0));
