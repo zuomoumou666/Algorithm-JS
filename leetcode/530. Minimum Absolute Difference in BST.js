9ffffa200708b9937515453c1d3b31492bda5cb7
zuomoumou666 <zuomoumou666@gmail.com>
2021-04-08 23:58:46 +0800

    530. Minimum Absolute Difference in BST

--- leetcode/530. Minimum Absolute Difference in BST.js
+++ leetcode/530. Minimum Absolute Difference in BST.js
@@ -37,20 +37,36 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var getMinimumDifference = function (root) {
-  if (!root || !root.left && !root.right) return Infinity;
-  let l = Infinity;
-  let r = Infinity;
-  if (root.left) l = Math.abs((getLastNode(root.left, 'right') || root.left).val - root.val);
-  if (root.right) r = Math.abs((getLastNode(root.right, 'left') || root.right).val - root.val);
-  return Math.min(getMinimumDifference(root.left), getMinimumDifference(root.right), l, r);
-};
+// var getMinimumDifference = function (root) {
+//   if (!root || !root.left && !root.right) return Infinity;
+//   let l = Infinity;
+//   let r = Infinity;
+//   if (root.left) l = Math.abs((getLastNode(root.left, 'right') || root.left).val - root.val);
+//   if (root.right) r = Math.abs((getLastNode(root.right, 'left') || root.right).val - root.val);
+//   return Math.min(getMinimumDifference(root.left), getMinimumDifference(root.right), l, r);
+// };
 
-function getLastNode(root, direction) {
-  if (!root) return null;
-  if (root[direction]) return getLastNode(root[direction], direction);
-  return root[direction] || root;
-}
+// function getLastNode(root, direction) {
+//   if (!root) return null;
+//   if (root[direction]) return getLastNode(root[direction], direction);
+//   return root[direction] || root;
+// }
+
+var getMinimumDifference = function (root) {
+  var prev = Infinity;
+  var totalMin = Infinity;
+  function traverse(node) {
+    if (!node) {
+      return;
+    }
+    traverse(node.left);
+    totalMin = Math.min(totalMin, Math.abs(node.val - prev));
+    prev = node.val;
+    traverse(node.right);
+  }
+  traverse(root);
+  return totalMin;
+};
 
 function TreeNode(val, left, right) {
   this.val = (val === undefined ? 0 : val)
@@ -59,27 +75,27 @@ function TreeNode(val, left, right) {
 }
 
 // [236,104,701,null,227,null,911]
-// const root = new TreeNode(
-//   236,
-//   new TreeNode(
-//     104,
-//     null,
-//     new TreeNode(227)
-//   ),
-//   new TreeNode(
-//     701,
-//     null,
-//     new TreeNode(911),
-//   )
-// );
 const root = new TreeNode(
-  1,
-  null,
+  236,
   new TreeNode(
-    3,
-    new TreeNode(2),
+    104,
+    null,
+    new TreeNode(227)
+  ),
+  new TreeNode(
+    701,
+    null,
+    new TreeNode(911),
   )
 );
+// const root = new TreeNode(
+//   1,
+//   null,
+//   new TreeNode(
+//     3,
+//     new TreeNode(2),
+//   )
+// );
 
 // console.log(getLastNode(root, 'right'));
 console.log(getMinimumDifference(root));
